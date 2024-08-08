2625480ea080b6518414367bca8a311b6ac09cc2
santoshtiwari41 <santoshhh066@gmail.com>
2024-08-08 17:46:40 +0545

    html pages render and css added

--- first/first/views.py
+++ first/first/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("Hello, world! This is my first Django app.")
+    # return HttpResponse("Hello, world! This is my first Django app.")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("This is the about page.")
